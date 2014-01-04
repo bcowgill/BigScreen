55fed97528be992f30da4aeef748fee533e8a87c
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-01-04 23:12:37 +0000

    Renamed show-metrics.sh to show-bigscreen.sh

--- go.sh
+++ go.sh
@@ -4,8 +4,6 @@
 # Generate the .js file listing all pictures to show
 ./make-auto-pics.sh
 
-# Fires up the browser to show the metrics
-./show-metrics.sh
-
-echo "If the tab with cranmer.ft.com is not working, log in as root and use the go.sh script to start apache after adjusting the IP addresses that script tells you about."
+# Fires up the browser to show the bigscreen display
+./show-bigscreen.sh
 
