8b5930c9b876f7e983798b71364228352a214b31
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-01-04 23:32:07 +0000

    Removed backup.sh and setup.sh scripts which are not relevant

--- backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DROPBOX=/Users/brent.cowgill/Dropbox/Photos/WorkSafe/_FT-docs
-pushd ..
-tar cvzf big-screen.tgz --exclude=.git/ big-screen
-tar xvzf big-screen.tgz --directory=$DROPBOX
-popd
