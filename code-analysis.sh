fc2fe127ee29efabe2ede559f0495f8412e5f65e
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-01-19 18:05:57 +0000

    Removed various legacy files in preparation for release

--- code-analysis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Perform code complexity/maintainability analysis
-pushd ..
-plato -r --exclude 'big-screen/(js|slide-panel)/' -d plato-output/big-screen big-screen
-open -a 'Google Chrome' plato-output/big-screen/index.html &
-popd
--- sandbox/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BROWSER=firefox
-#BROWSER=chromium-browser
-#$BROWSER testme/website/ft-mobile/trunk/src/gwt/ft-ad-enablement/war/QUnitChainer/0-control.html &
-$BROWSER "http://127.0.0.1:8080/jscoverage.html?missing=true&frame=QUnitChainer/0-control.html" "http://127.0.0.1:8080/jscoverage.html?missing=true&frame=QUnitChainer/0-monitor.html" &
