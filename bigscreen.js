6df4657b09a3a4560de149f07558db4f4b8481cd
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-02-08 20:58:22 +0000

    Moved Effects configuration to separate file

--- bigscreen.js
+++ bigscreen.js
@@ -3,7 +3,7 @@
 
 /*jslint browser: true, plusplus: true, maxerr: 1000, indent: 3 */
 /*global
-   AutoPics, Math, SETTINGS, jQuery, hideTopPanel: true, URLs, companyURLs, miscURLs, meetingURLs, timeForMeeting
+   AutoPics, Math, SETTINGS, jQuery, hideTopPanel: true, URLs, companyURLs, miscURLs, meetingURLs, timeForMeeting, Effects
 */
 /*properties
     '-', CHANGETIME, CHANGETME, DEFAULT, DROPBOX, EFFECTTIME, FIXEDEFFECT,
@@ -26,55 +26,6 @@ var loadedURL;
 var effectUsed;
 
 var artURLs = [];
-
-// Documentation for Effects:
-// http://api.jqueryui.com/category/effects/
-var Effects = {
-   'none': {},
-   'blind': { direction: "up" },
-   'blind2': { direction: "down" },
-   'blind3': { direction: "left" },
-   'blind4': { direction: "right" },
-   'blind5': { direction: "vertical" },
-   'blind6': { direction: "horizontal" },
-   'bounce': {},
-   'clip': { direction: "vertical" },
-   'clip2': { direction: "horizontal" },
-   'drop': { direction: "left" },
-   'drop2': { direction: "right" },
-   'drop3': { direction: "up" },
-   'drop4': { direction: "down" },
-   'explode': { pieces: 9 },
-   'explode2': { pieces: 4 },
-   'explode3': { pieces: 16 },
-   'fade': {}, // new
-   'fold': { size: 200 },
-   'fold2': { size: 200, horizFirst: true },
-   'highlight': { color: "red" }, // only works well for images smaller than the viewport
-/*
-   'highlight3': { color: "yellow" },
-   'highlight4': { color: "green" },
-   'highlight5': { color: "blue" },
-   'highlight6': { color: "violet" },
-   'highlight7': { color: "black" },
-*/
-   'puff': { percent: 150 },
-   'pulsate': { times: 3 },
-   'scale': { percent: 0 }, // default center
-   'scale2': { percent: 0, origin: ["bottom", "right"] },
-   'scale3': { percent: 0, origin: ["bottom", "left"] },
-   'scale4': { percent: 0, origin: ["top", "right"] },
-   'scale5': { percent: 0, origin: ["top", "left"] },
-   'shake': { times: 5 },
-   'shake2': { direction: "up", times: 5 },
-   //'size': { to: { width: 0, height: 200 }, origin: ["bottom", "right"], scale: "content" },
-   'slide': { direction: "left" },
-   'slide2': { direction: "up" },
-   'slide3': { direction: "down" },
-   'slide4': { direction: "right" },
-
-   '-': []
-};
 
 function log() {
    if (window.console && window.console.log && SETTINGS.LOG) {
@@ -311,7 +262,7 @@ function start() {
    help();
    var timer = setInterval(function () {
       log('Interval timer ' + timer);
-      if (!SETTINGS.DEFAULT && URLs) {
+      if (!SETTINGS.DEFAULT && URLs && Effects) {
          if (timer) {
             clearInterval(timer);
             timer = 0;
